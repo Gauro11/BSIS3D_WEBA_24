8e35636b910117b6b1b2cbb0b9a735dd425befea
jaypeepualengco6 <jaypeepualengco06@gmail.com>
2024-03-22 12:11:16 +0800

    latest

--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -2,6 +2,12 @@
 
 require 'functions.php';
 require 'App.php';
+require 'controller.php';
+require 'Model.php';
+require 'App.php';
 
+spl_autoload_register(function ($class_name){}
+
+require'../app/models' . $class_name . 'php' ;);
 
 ?>
