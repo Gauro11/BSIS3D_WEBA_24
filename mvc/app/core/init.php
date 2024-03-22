ee7ef9507babc5da00fdf38b0d7e141e718bf5b4
ivor <ivorcruz19@gmail.com>
2024-03-22 12:35:19 +0800

    dnewdn

--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -5,8 +5,8 @@ require 'App.php';
 require 'controller.php';
 require 'Model.php';
 
-spl_autoload_register(function ($class_name){}
+spl_autoload_register(function ($class_name){
 
 require'../app/models' . $class_name . 'php' ;);
-
+}
 ?>
